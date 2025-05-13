788519deab89f332e6b2cbfb8d84fff6e60faeee
mlhsn <mahmudul.hasan@monstar-lab.com>
2025-05-13 13:06:22 +0600

    feat: finalize app logic

--- asgi.py
+++ asgi.py
@@ -1,20 +1,22 @@
 """
 Weather server module
 """
-import icecream
+import httpx
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     openapi_url: str
+    weatherapi_key: str
+    weatherapi_url: str = "http://api.weatherapi.com/v1/current.json"
 
     model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
 app = FastAPI(
-    openapi_url=settings.openapi_url, # disable API doc
+    openapi_url=settings.openapi_url,  # disable API doc
 )
 
 
@@ -25,4 +27,14 @@ async def read_root():
 
 @app.get("/weather/{city_id}")
 async def read_item(city_id: str = "Dhaka"):
-    return {"city_id": city_id}
+    async with httpx.AsyncClient() as cli:
+        response: httpx.Response = await cli.get(settings.weatherapi_url, params={
+            "key": settings.weatherapi_key,
+            "q": city_id,
+            "lang": "bn",
+        })
+
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            return response.json()
